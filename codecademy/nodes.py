de8391ab150f011cd788e08f0aee5d4c496859f2
Bastien <bastien.ratat@gmail.com>
2019-07-15 20:24:50 +0800

    What are nodes in Python?

--- codecademy/nodes.py
+++ codecademy/nodes.py
@@ -1,5 +1,11 @@
 
 # NODE INTRODUCTION
 
-#Nodes are the fundamental building block of many computer science data structures. They form the basis for linked lists, stacks, queues, trees, and more.
-#An individual node contains data and links to other nodes. Each data structure adds additional constraints or behavior to these features to create the desired structure.
+# Nodes are the fundamental building block of many computer science data structures. They form the basis for linked lists, stacks, queues, trees, and more.
+# An individual node contains data and links to other nodes. Each data structure adds additional constraints or behavior to these features to create the desired structure.
+# The data contained within a node can be a variety of types. It could be an integer (5), a string ("five"), decimal (5.1), an array ([5,3,4]) or nothing (null).
+# The link or links within the node are sometimes referred to as pointers. This is because they “point” to another node.
+# Typically, data structures implement nodes with one or more links. If these links are null, it denotes that you have reached the end of the particular node or link path you were previously following.
+# Often, due to the data structure, nodes may only be linked to from a single other node.
+# If you inadvertently remove the single link to a node, that node’s data and any linked nodes could be “lost” to your application. When this happens to a node, it is considered an orphaned node.
+
