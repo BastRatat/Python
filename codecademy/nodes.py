f60a53c0573ef96d4fe3e5e98e6813643d2f9c8c
Bastien <bastien.ratat@gmail.com>
2019-07-15 20:42:55 +0800

    Create getters and setters + example

--- codecademy/nodes.py
+++ codecademy/nodes.py
@@ -9,3 +9,41 @@
 # Often, due to the data structure, nodes may only be linked to from a single other node.
 # If you inadvertently remove the single link to a node, that node’s data and any linked nodes could be “lost” to your application. When this happens to a node, it is considered an orphaned node.
 
+
+# CLASSES
+class Node:
+    # class Node that takes a constructor with two properties : value and link_node (default is None for the link_node)
+  def __init__(self, value, link_node=None):
+    self.value = value
+    self.link_node = link_node
+
+    # create a get_value method that works on the object (self) and return the value.
+  def get_value(self):
+    return self.value
+
+    # create a get_link_node method that works on the object (self) and return the link_node.
+  def get_link_node(self):
+    return self.link_node
+
+    # create a setter method that allows to update the link on the node.
+  def set_link_node(self, link_node):
+    self.link_node = link_node
+
+# INSTANTIATION
+yacko = Node("likes to yak")
+wacko = Node("has a penchant for hoarding snacks")
+dot = Node("enjoys spending time in movie lots")
+
+# CALLS
+yacko.set_link_node(dot) # yacko has a pointer to dot.
+dot.set_link_node(wacko) # dot has a pointer to wacko.
+
+dots_data = yacko.get_link_node().get_value() # gets the link of dots and print its value.
+
+wackos_data = dot.get_link_node().get_value() # gets the link of wacko and print its value.
+
+print(dots_data)
+print(wackos_data)
+
+yacko_to_wacko = yacko.get_link_node().get_link_node().get_value()
+print(yacko_to_wacko)
