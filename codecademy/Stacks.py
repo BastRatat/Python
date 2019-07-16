a1dc270da4a5f907c23e03f03f1d6ae1696dfe90
Bastien <bastien.ratat@gmail.com>
2019-07-16 22:25:46 +0800

    Create our first stack class

--- codecademy/Stacks.py
+++ codecademy/Stacks.py
@@ -27,3 +27,44 @@
 #Can have a limited size
 #Pushing data onto a full stack results in a stack overflow
 #Stacks process data Last In, First Out (LIFO)
+
+from node import Node
+
+class Stack:
+  def __init__(self, limit=1000):
+    self.top_item = None
+    self.size = 0
+    self.limit = limit
+  
+  # This first method will instantiate a node from Node, set that node to be the current top item on the stack on set the stack instance's top item equal to the new item. 
+  def push(self, value):
+    if self.has_space():
+      item = Node(value)
+      item.set_next_node(self.top_item)
+      self.top_item = item
+      self.size += 1
+      print("Adding {} to the pizza stack!".format(value))
+    else:
+      print("No room for {}!".format(value))
+
+      # This method will create a variable items_to_remove equal to the top item of the stack, will set a new top item (since we are removing our stack first item) and return the value of the item to be removed.
+  def pop(self):
+    if not self.is_empty():
+      item_to_remove = self.top_item
+      self.top_item = item_to_remove.get_next_node()
+      self.size -= 1
+      print("Delivering " + item_to_remove.get_value())
+      return item_to_remove.get_value()
+    print("All out of pizza.")
+
+  def peek(self):
+    if not self.is_empty():
+      return self.top_item.get_value()
+    print("Nothing to see here!")
+
+  def has_space(self):
+    return self.limit > self.size
+
+  def is_empty(self):
+    return self.size == 0
+  
