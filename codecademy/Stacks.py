ca6979648438da27de1609babf048cf78059379f
Bastien <bastien.ratat@gmail.com>
2019-07-16 21:33:36 +0800

    Stacks introduction

---
+++ codecademy/Stacks.py
@@ -0,0 +1,29 @@
+#A stack is a data structure which contains an ordered set of data.
+
+#Stacks provide three methods for interaction:
+
+#Push - adds data to the “top” of the stack
+#Pop - returns and removes data from the “top” of the stack
+#Peek - returns data from the “top” of the stack without removing it
+
+#This is a Last In, First Out or LIFO structure.
+
+# Depending on the implementation, the top of the stack is equivalent to the head node of a linked list and the bottom of the stack is equivalent to the tail node.
+
+#A constraint that may be placed on a stack is its size. This is done to limit and quantify the resources the data structure will take up when it is “full”.
+
+#Attempting to push data onto an already full stack will result in a stack overflow. 
+
+# SUM UP
+
+#Stacks:
+
+#Contain data nodes
+#Support three main operations
+#Push adds data to the top of the stack
+#Pop removes and provides data from the top of the stack
+#Peek reveals data on the top of the stack
+#Implementations include a linked list or array
+#Can have a limited size
+#Pushing data onto a full stack results in a stack overflow
+#Stacks process data Last In, First Out (LIFO)
