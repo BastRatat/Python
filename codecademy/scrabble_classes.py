1d1b42981ad443f897b4e4ad1d4ab34743e5f978
Bastien <bastien.ratat@gmail.com>
2019-07-21 11:45:45 +0800

    upper() method applied on the input. Each word can be played now.

--- codecademy/scrabble_classes.py
+++ codecademy/scrabble_classes.py
@@ -4,8 +4,7 @@ x = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", 
 y = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 print("Input a word to play scrabble")
 word = input("")
-game = Game(x, y, word)
-letter_to_points = game.letter_to_point()
+
 print(letter_to_points)
 # CLASSES DECLARATION
 
@@ -28,7 +27,7 @@ class Game:
 class Calculations(Game):
 
     def __init__(self, word):
-        self.word = word
+        self.word = word.upper()
 
     def score_word(self):
         point_total = 0
@@ -39,10 +38,12 @@ class Calculations(Game):
 
 
 # INSTANTIATIONS AND CALLS 
-print("You chose to find out the how many points you'll get from {}".format(word))
+game = Game(x, y, word)
+letter_to_points = game.letter_to_point()
+print("You chose to find out how many points you'll get from {}".format(word))
 
 # print(game) # returns the repr() method.
 
 #print(letter_to_points)
-test = Calculations(word)
-print(test.score_word())
+score = Calculations(word)
+print(score.score_word())
