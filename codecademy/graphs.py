90c487e7de2d45b860b2dc67d8a1589faab9cf95
Bastien <bastien.ratat@gmail.com>
2019-08-01 20:31:52 +0800

    graph terminologies

---
+++ codecademy/graphs.py
@@ -0,0 +1,22 @@
+#Graphs are the perfect data structure for modeling networks, which make them an indispensable piece of your data structure toolkit. They’re composed of nodes, or vertices, which hold data, and edges, which are a connection between two vertices. A single node is a vertex.
+
+#Consider a map of the area where you live. As a graph, we could model bus stops as vertices, with bus routes between stops functioning as the edges.
+
+#What about the internet? Web pages can be vertices, and the hyperlinks which connect them are edges.
+
+#Graphs have varying degrees of connection. The higher the ratio of edges to vertices, the more connected the graph.
+
+#Graphs are an essential data structure in computer science for modeling networks. Let’s review some key terms:
+
+#vertex: A node in a graph.
+#edge: A connection between two vertices.
+#adjacent: When an edge exists between vertices.
+#path: A sequence of one or more edges between vertices.
+#disconnected: Graph where at least two vertices have no path connecting them.
+#weighted: Graph where edges have an associated cost.
+#directed: Graph where travel between vertices can be restricted to a single direction.
+#cycle: A path which begins and ends at the same vertex.
+#adjacency matrix: Graph representation where vertices are both the rows and the columns. Each cell represents a possible edge.
+#adjacency list: Graph representation where each vertex has a list of all the vertices it shares an edge with.
+
+
