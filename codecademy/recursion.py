b2439da40604ddc2282f7f4f6c705ef9079028d8
Bastien <bastien.ratat@gmail.com>
2019-08-06 00:24:17 +0800

    Recursion : conceptual.

---
+++ codecademy/recursion.py
@@ -0,0 +1,18 @@
+
+#Recursion has two fundamental aspects: the base case and the recursive step.
+#The base case dictates whether the function will recurse, or call itself. Without a base case, it’s the iterative equivalent to writing an infinite loop.
+
+#Because we’re using a call stack to track the function calls, your computer will throw an error due to a stack overflow if the base case is not sufficient.
+
+#The other fundamental aspect of a recursive function is the recursive step. This portion of the function is the step that moves us closer to the base case.
+
+#In a recursive function, the “counting variable” equivalent is the argument to the recursive call.
+
+#Analyzing the Big O runtime of a recursive function is very similar to analyzing an iterative function. Substitute iterations of a loop with recursive calls.
+
+ 
+
+
+
+
+
