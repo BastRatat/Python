bb383dc75c0e9434e1b51345f6a6b03f893bd148
Bastien <bastien.ratat@gmail.com>
2019-08-22 22:36:47 +0800

    Binary search : conceptual and uses

--- codecademy/search_algorithms.py
+++ codecademy/search_algorithms.py
@@ -1,5 +1,7 @@
 
-# SEARCH ALGOS
+# SEARCH ALGOS - conceptuals
+
+# 1. LINEAR SEARCH
 
 #Linear search is a search algorithm that sequentially checks whether a given value is an element of a specified list by scanning the elements of a list one-by-one until it finds the target value.
 
@@ -11,6 +13,24 @@
 #Linear search is a good choice for a search algorithm when:
 #    - You expect the target value to be positioned near the beginning of the list.
 #    - A search needs to be performed on an unsorted list because linear search traverses the entire list from beginning to end, regardless of its order.
+
+#Without any knowledge about the ordering, we would resort to a linear search taking O(N) time.
+
+#2. BINARY SEARCH
+
+# In each iteration, we are cutting the list in half. The time complexity is O(log N)
+# For example, a sorted list of 64 elements will take at most log2(64) = 6 comparisons.
+
+
+
+
+
+
+
+
+
+
+
 
 
 
